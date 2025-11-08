f828bfb10612dd2951c0162c73639c80f11a8159
Pratik Dheringe <pratikdheringe@gmail.com>
2025-11-08 14:06:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2973195743-3aa4a",
+  "appId": "1:698752409803:web:6a4227cad6522e56fdbb4e",
+  "apiKey": "AIzaSyDAjJEY7S8FW2fVzXilK6KoabhyQp9NSz0",
+  "authDomain": "studio-2973195743-3aa4a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "698752409803"
+};
